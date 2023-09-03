e6da5c6df16ddf4f72c0a3083e03ffbb4bf8e3f4
kurnakovv <maksk210@gmail.com>
2023-09-03 15:34:40 +0900

    Disable all tests

--- HowToStoreVariablesTest/IntegrationTests/AppsettingsTests.cs
+++ HowToStoreVariablesTest/IntegrationTests/AppsettingsTests.cs
@@ -1,39 +1,39 @@
-﻿using Microsoft.Extensions.Configuration;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿//using Microsoft.Extensions.Configuration;
+//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
 
-namespace HowToStoreVariablesTest.IntegrationTests
-{
-    public class AppsettingsTests
-    {
-        private readonly IConfiguration _configuration;
+//namespace HowToStoreVariablesTest.IntegrationTests
+//{
+//    public class AppsettingsTests
+//    {
+//        private readonly IConfiguration _configuration;
 
-        public AppsettingsTests()
-        {
-            var builder = new ConfigurationBuilder()
-                .AddEnvironmentVariables()
-                .AddJsonFile("appsettings.json")
-                .AddJsonFile("appsettings.Mock.json");
-            _configuration = builder.Build();
-        }
+//        public AppsettingsTests()
+//        {
+//            var builder = new ConfigurationBuilder()
+//                .AddEnvironmentVariables()
+//                .AddJsonFile("appsettings.json")
+//                .AddJsonFile("appsettings.Mock.json");
+//            _configuration = builder.Build();
+//        }
 
-        [Fact]
-        public void GetTestConstant()
-        {
-            string testConts = _configuration["TestConstant"];
-            Assert.NotNull(testConts);
-            Assert.Equal("This is my test constant from appsettings.json", testConts);
-        }
+//        [Fact]
+//        public void GetTestConstant()
+//        {
+//            string testConts = _configuration["TestConstant"];
+//            Assert.NotNull(testConts);
+//            Assert.Equal("This is my test constant from appsettings.json", testConts);
+//        }
 
-        [Fact]
-        public void GetMockConst()
-        {
-            string mockConst = _configuration["MockConst"];
-            Assert.NotNull(mockConst);
-            Assert.Equal("This is mock value", mockConst);
-        }
-    }
-}
+//        [Fact]
+//        public void GetMockConst()
+//        {
+//            string mockConst = _configuration["MockConst"];
+//            Assert.NotNull(mockConst);
+//            Assert.Equal("This is mock value", mockConst);
+//        }
+//    }
+//}
