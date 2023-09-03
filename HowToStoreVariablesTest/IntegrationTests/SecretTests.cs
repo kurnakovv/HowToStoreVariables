d6143c06bb49e3334930ef5fd164e4f5b82d93d4
kurnakovv <maksk210@gmail.com>
2023-09-03 15:39:04 +0900

    Enable secret tests

--- HowToStoreVariablesTest/IntegrationTests/SecretTests.cs
+++ HowToStoreVariablesTest/IntegrationTests/SecretTests.cs
@@ -1,28 +1,28 @@
-﻿//using Microsoft.Extensions.Configuration;
-//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Text;
-//using System.Threading.Tasks;
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-//namespace HowToStoreVariablesTest.IntegrationTests
-//{
-//    public class SecretTests
-//    {
-//        private readonly IConfiguration _configuration;
+namespace HowToStoreVariablesTest.IntegrationTests
+{
+    public class SecretTests
+    {
+        private readonly IConfiguration _configuration;
 
-//        public SecretTests()
-//        {
-//            var builder = new ConfigurationBuilder()
-//                .AddUserSecrets<Program>();
-//            _configuration = builder.Build();
-//        }
+        public SecretTests()
+        {
+            var builder = new ConfigurationBuilder()
+                .AddUserSecrets<Program>();
+            _configuration = builder.Build();
+        }
 
-//        [Fact]
-//        public void Get_TEST_SECRET()
-//        {
-//            string secret = _configuration["TEST_SECRET"];
-//            Assert.NotNull(secret);
-//        }
-//    }
-//}
+        [Fact]
+        public void Get_TEST_SECRET()
+        {
+            string secret = _configuration["TEST_SECRET"];
+            Assert.NotNull(secret);
+        }
+    }
+}
