06003c45cb5f37169fdff202f010064837fa341a
kurnakovv <maksk210@gmail.com>
2023-09-03 16:17:35 +0900

    Enable env tests

--- HowToStoreVariablesTest/IntegrationTests/EnvironmentTests.cs
+++ HowToStoreVariablesTest/IntegrationTests/EnvironmentTests.cs
@@ -1,29 +1,29 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Text;
-//using System.Threading.Tasks;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-//namespace HowToStoreVariablesTest.IntegrationTests
-//{
-//    public class EnvironmentTests : IDisposable
-//    {
-//        public EnvironmentTests()
-//        {
-//            Environment.SetEnvironmentVariable("TestVariable", "This is test variable from integration test");
-//        }
+namespace HowToStoreVariablesTest.IntegrationTests
+{
+    public class EnvironmentTests //: IDisposable
+    {
+        //public EnvironmentTests()
+        //{
+        //    Environment.SetEnvironmentVariable("TestVariable", "This is test variable from integration test");
+        //}
 
-//        public void Dispose()
-//        {
-//            Environment.SetEnvironmentVariable("TestVariable", null);
-//        }
+        //public void Dispose()
+        //{
+        //    Environment.SetEnvironmentVariable("TestVariable", null);
+        //}
 
-//        [Fact]
-//        public void GetEnvVariable()
-//        {
-//            string result = Environment.GetEnvironmentVariable("TestVariable");
-//            Assert.NotNull(result);
-//            Assert.Equal("This is test variable from integration test", result);
-//        }
-//    }
-//}
+        [Fact]
+        public void GetEnvVariable()
+        {
+            string result = Environment.GetEnvironmentVariable("TestVariable");
+            Assert.NotNull(result);
+            Assert.Equal("This is test variable from integration test", result);
+        }
+    }
+}
