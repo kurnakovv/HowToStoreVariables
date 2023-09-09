93766d206614ae0c140e802e4816193ea61c0a60
kurnakovv <maksk210@gmail.com>
2023-09-09 15:30:01 +0900

    Add HostingController

---
+++ HowToStoreVariables/Controllers/HostingController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HowToStoreVariables.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HostingController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+
+        public HostingController(
+            IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpGet]
+        public string Get()
+        {
+            return Environment.GetEnvironmentVariable("TEST_SECRET") ?? _configuration["TEST_SECRET"];
+        }
+    }
+}
